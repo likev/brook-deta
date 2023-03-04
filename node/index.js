b53fe90c1e0cb9f14538389ad5e6160e4f937e16
likev <3146974+likev@users.noreply.github.com>
2023-03-04 13:50:20 +0000

    test ws timeout

--- node/index.js
+++ node/index.js
@@ -3,12 +3,18 @@ import { WebSocketServer } from 'ws';
 const wss = new WebSocketServer({ port: process.env.PORT });
 
 wss.on('connection', function connection(ws) {
-  ws.on('error', console.error);
+    ws.on('error', console.error);
 
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
-    ws.send(data);
-  });
+    ws.on('message', function message(data) {
+        console.log('received: %s', data);
+        ws.send(data);
+    });
 
-  ws.send('something');
+    let count = 0, interval = 5; //5s
+
+    setInterval(() => {
+        count++;
+        ws.send({ count, time: interval * count });
+    }, interval);
+
 });
